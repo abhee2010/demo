dce73670341944217fb8d30a217ab5175eca48f3
abhee2010 <abhee2010@gmail.com>
2020-04-08 00:36:50 +0530

    added welcome.html (UI part)

--- src/main/java/com/shailu/practice/demo/controller/CustomerController.java
+++ src/main/java/com/shailu/practice/demo/controller/CustomerController.java
@@ -3,20 +3,29 @@ package com.shailu.practice.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import org.springframework.web.bind.annotation.RestController;
-
 import com.shailu.practice.demo.entity.Customer;
-import com.shailu.practice.demo.repository.CustomerRepository;
 import com.shailu.practice.demo.service.CustomerService;
 
-@RestController
+@Controller
 @RequestMapping("/home")
 public class CustomerController {
 
+	@Value("${welcome.message}")
+    private String message;
+	
 	@Autowired
 	private CustomerService customerService;
+	
+	@RequestMapping("/welcome")
+    public String main(Model model) {
+        model.addAttribute("message", message);
+        return "welcome"; //view
+    }
 	
 	@RequestMapping("/customers")
 	public List<Customer> getCustomers() {
--- src/main/java/com/shailu/practice/demo/dao/CustomerDAO.java
+++ src/main/java/com/shailu/practice/demo/dao/CustomerDAO.java
@@ -2,8 +2,6 @@ package com.shailu.practice.demo.dao;
 
 import java.util.List;
 
-import org.springframework.stereotype.Repository;
-
 import com.shailu.practice.demo.entity.Customer;
 
 public interface CustomerDAO {
